dbee53c05805114750ecbfb903d6302caac2a5a8
hashmiusama <hashmiusama@hotmail.com>
2015-03-27 21:00:04 +0500

    Update BoW.R

--- BoW.R
+++ BoW.R
@@ -1,22 +1,15 @@
 library(tm) #load text mining library
-#library(qdap) #load bag of words library
 file_list = list.files("/Rprog/myTexts") #location of files 
 obj_list = lapply("/Rprog/myTexts/random.txt", readLines) #read text lines from files
 obj_vec <- as.vector(obj_list) #storing text data as vectors
-a  <-Corpus(VectorSource(obj_vec), readerControl = list(language="lat")) #converting vector into corpus
-#summary(a)  #for testing purpose
+a <- Corpus(VectorSource(obj_vec), readerControl = list(language="lat")) #converting vector into corpus
 a <- tm_map(a, removeNumbers) #remove numbers from text
 a <- tm_map(a, removePunctuation) #remove punctuation from text
-a <- tm_map(a , stripWhitespace) #remove whitespaces from text
+a <- tm_map(a, stripWhitespace) #remove whitespaces from text
 a <- tm_map(a, tolower) #convert all text to lowercase
 a <- tm_map(a, removeWords, stopwords("english")) #remove stopwords 
-# a <- tm_map(a, stemDocument, language = "english")  #setting document type
-# for(i in seq(1:1)){
-#  writeLines(a[[i]])  #for testing purpose
-# }
 a <- tm_map(a, PlainTextDocument) #settting document type to make convertible to matrix
-adtm <-DocumentTermMatrix(a) #making document term matrix (BoW)
-#inspect(adtm[1,1:500]) #testing words from 1 to 500 less stopwords
-adtm <- removeSparseTerms(adtm, 0.5) #removing sparse terms below 50% sparcity
+adtm <-TermDocumentMatrix(a) #making document term matrix (BoW)
+adtm <- removeSparseTerms(adtm, 0.75) #removing sparse terms below 75% sparcity
 findFreqTerms(adtm, 10) #find words with frequency 10
 inspect(adtm)
